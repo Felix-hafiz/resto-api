2c6529c20947558039ae97f40255a50521c96fcf
Felix Hafiz <felixhafizjulianto354@gmail.com>
2023-04-30 22:34:42 +0700

    feat: add more routes handler for detail user
    add Get route for single user
    add PUT route for single user
    add DELETE route for single user

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -14,8 +14,22 @@ router
         const data = await User.create({ name })
         res.status(201).send(data)
     })
+
+router
+    .route('/users/:id')
+    .get(async (req: Request, res: Response) => {
+        const data = await User.findById(req.params.id)
+        res.send(data)
+    })
+    .put(async (req: Request, res: Response) => {
+        const data = await User.findByIdAndUpdate(req.params.id, req.body, {
+            returnDocument: 'after',
+        })
+        res.send({ message: 'Succes Updating User!', data })
+    })
     .delete(async (req: Request, res: Response) => {
-        res.send('user delete route')
+        const data = await User.findByIdAndDelete(req.params.id)
+        res.send({ message: 'Succes Deleting User!', data })
     })
 
 export default router
