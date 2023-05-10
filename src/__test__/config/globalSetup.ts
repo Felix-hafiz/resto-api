44816cb498e8ad841d57401d547bfe5f1aea942c
Felix Hafiz <felixhafizjulianto354@gmail.com>
2023-05-10 21:41:21 +0700

    test: add setup config for jest

---
+++ src/__test__/config/globalSetup.ts
@@ -0,0 +1,20 @@
+import { MongoMemoryServer } from 'mongodb-memory-server'
+import mongoose from 'mongoose'
+import config from './config'
+
+export default async function globalSetup() {
+    if (config.Memory) {
+        const mongod: MongoMemoryServer = await MongoMemoryServer.create()
+        const uri = mongod.getUri()
+
+        /* eslint @typescript-eslint/no-explicit-any: ["off"] */
+        ;(global as any).__MONGOINSTANCE = mongod
+        process.env.MONGODB_URI = uri.slice(0, uri.lastIndexOf('/'))
+    } else {
+        process.env.MONGODB_URI = `mongodb://${config.IP}:${config.Port}`
+    }
+
+    await mongoose.connect(`${process.env.MONGODB_URI}/${config.Database}`)
+    await mongoose.connection.db.dropDatabase()
+    await mongoose.disconnect()
+}
